5b4a28c981f4dad19f96077197f9f587067dba30
Adam Skorupski <adamskorupski@hotmail.com>
2023-11-17 22:22:17 +0100

    first try to extract strings from prompt

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <fmt/core.h>
+#include <fmt/ranges.h>
 #include <string>
 #include <iostream>
+#include <vector>
+
+struct query_element {
+
+};
 
 auto main() -> int {
     fmt::println("Welcome to Based - database in C++");
@@ -8,8 +14,21 @@ auto main() -> int {
     while (!quit) {
         auto prompt = std::string();
         fmt::print(">");
-        std::cin >> prompt;
+        std::getline(std::cin, prompt);
 
-        fmt::println("{}",prompt);
+        auto split_prompt = std::vector<std::string>();
+        auto prompt_part = std::string();
+
+        for (auto c : prompt) {
+            if (c == '"') {
+                split_prompt.push_back(prompt_part);
+                prompt_part = std::string();
+            }
+
+            prompt_part.push_back(c);
+        }
+        split_prompt.push_back(prompt_part);
+
+        fmt::println("{}", split_prompt);
     }
 }
