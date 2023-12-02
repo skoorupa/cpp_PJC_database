45a671d6ade0000e02a1e41be404850b0fd2a8ce
Adam Skorupski <adamskorupski@hotmail.com>
2023-12-02 23:04:16 +0100

    added column + row basic logic

--- db/table.cpp
+++ db/table.cpp
@@ -1,19 +1,36 @@
 #include "table.hpp"
+#include <fmt/core.h>
 
 namespace db {
     /////////////////////////////////////
     // COLUMN CLASS
 
-    Column::Column(const std::string &name, DBType type, int id) : name(name), type(type), id(id) {}
+    Column::Column(int id, const std::string &name, DBType type) : id(id), name(name), type(type) {}
+
+    int Column::getId() const {return id;}
+    const std::string &Column::getName() const {return name;}
+    DBType Column::getType() const {return type;}
+
+    /////////////////////////////////////
+    // ROW CLASS
+
+    Row::Row(int id, const std::map<int, std::string> &values) : id(id), values(values) {}
 
     /////////////////////////////////////
     // TABLE CLASS
 
     Table::Table(const std::string &name) : name(name), columns(std::deque<Column>()) {
         curr_column_id = 1;
+        curr_row_id = 1;
     }
 
     auto Table::add_column(std::string name, DBType type) {
-        columns.push_back(Column(name, type, curr_column_id));
+        columns.push_back(Column(curr_column_id, name, type));
+        curr_column_id++;
+    }
+
+    auto Table::add_row(std::map<int, std::string> values) {
+        rows.push_back(Row(curr_row_id, values));
+        curr_row_id++;
     }
 };
--- db/table.hpp
+++ db/table.hpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <deque>
+#include <map>
 
 namespace db {
     /////////////////////////////////////
@@ -14,9 +15,22 @@ namespace db {
         int id;
         std::string name;
         DBType type;
+    public:
+        Column(int id, const std::string &name, DBType type);
+
+        int getId() const;
+        const std::string &getName() const;
+        DBType getType() const;
+    };
+
+    /////////////////////////////////////
+    // ROW CLASS
+    class Row {
+        int id;
+        std::map<int, std::string> values; // int - column_id, string - value
 
     public:
-        Column(const std::string &name, DBType type, int id);
+        Row(int id, const std::map<int, std::string> &values);
     };
 
     /////////////////////////////////////
@@ -24,11 +38,15 @@ namespace db {
     class Table {
         std::string name;
         std::deque<Column> columns;
-        int curr_column_id;
+        std::deque<Row> rows;
+        int curr_column_id; // wewnetrzne id dla programu
+        int curr_row_id;  // wewnetrzne id dla programu
 
     public:
         Table(const std::string &name);
 
         auto add_column(std::string name, DBType type);
+        auto add_row(std::map<int, std::string> values);
+        auto print();
     };
 };
