c82b93e6a301ccce6b307a2332cd40f53e889dc7
Adam Skorupski <adamskorupski@hotmail.com>
2023-12-02 22:32:18 +0100

    added id to column

--- db/table.cpp
+++ db/table.cpp
@@ -4,13 +4,16 @@ namespace db {
     /////////////////////////////////////
     // COLUMN CLASS
 
-    Column::Column(const std::string &name, DBType type) : name(name), type(type) {}
+    Column::Column(const std::string &name, DBType type, int id) : name(name), type(type), id(id) {}
 
     /////////////////////////////////////
     // TABLE CLASS
 
-    Table::Table(const std::string &name) : name(name), columns(std::deque<Column>()) {}
+    Table::Table(const std::string &name) : name(name), columns(std::deque<Column>()) {
+        curr_column_id = 1;
+    }
+
     auto Table::add_column(std::string name, DBType type) {
-        columns.push_back(Column(name, type));
+        columns.push_back(Column(name, type, curr_column_id));
     }
 };
